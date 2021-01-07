95998b5bc492862bc00916f5b3b72b74dac5a9fc
victorpcastro <vitinhopaivinha@gmail.com>
2021-01-07 19:10:52 -0300

    Add footter

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,26 +1,30 @@
 import './styles.css';
-import {ReactComponent as MainImage} from './main.svg';
-function Home(){
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
+function Home() {
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br/> que entregamos <br/> até você!!!
-                    </h1>
-                    <h3 className="home-subtitle">
-                        escolha seu pedido e em poucos minutos <br/>
-                        levaremos ate sua porta
-                    </h3>
-                    <a href="orders" className=" home-btn-order" >
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="main-image">
-                    <MainImage />
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br /> que entregamos <br /> até você!!!
+                        </h1>
+                        <h3 className="home-subtitle">
+                            escolha seu pedido e em poucos minutos <br />
+                            levaremos ate sua porta
+                        </h3>
+                        <a href="orders" className=" home-btn-order" >
+                            FAZER PEDIDO
+                        </a>
+                    </div>
+                    <div className="main-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
